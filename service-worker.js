363c528adcade05890c42db78c7c5953976d99b7
sobol4156 <sobol4156@gmail.com>
2025-09-13 15:08:44 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,26 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("v1").then((cache) =>
-      cache.addAll(["/test-pwa-react/", "/index.html"])
+      cache.addAll([
+        "/test-pwa-react/",
+        "/test-pwa-react/index.html",
+        "/test-pwa-react/app.css",
+        "/test-pwa-react/home.js",
+      ])
     )
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(
-      (response) => response || fetch(event.request)
-    )
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((resp) => {
+        return caches.open("v1").then((cache) => {
+          cache.put(event.request, resp.clone());
+          return resp;
+        });
+      });
+    })
   );
 });
+
